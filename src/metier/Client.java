a035435a2adc7c202a002de5d617d5862884a6a3
SunnyP01 <sunny.purseed@ynov.com>
2017-03-10 16:38:13 +0100

    Ajout du conseiller dans Client

--- src/metier/Client.java
+++ src/metier/Client.java
@@ -2,8 +2,21 @@ package metier;
 
 public class Client extends User{
 
-	public Client(String email, String nom, String prenom, String password) {
+	private int idConseiller;
+	
+	public Client(String email, String nom, String prenom, String password, int idConseiller) {
 		super(email, nom, prenom, password);
 		// TODO Auto-generated constructor stub
+		this.idConseiller = idConseiller;
+	}
+	
+	public int getIdConseiller()
+	{
+		return this.idConseiller;
+	}
+	
+	public void setIdConseiller(int idConseiller)
+	{
+		this.idConseiller = idConseiller;
 	}
 }
